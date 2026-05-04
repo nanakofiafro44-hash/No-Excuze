3949f657c560096ebb7383f4cbb309a2327cc95e
nanakofiafro44-hash <nanakofiafro44@gmail.com>
2026-05-04 06:27:40 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NoExcuze from './NoExcuze'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NoExcuze />
+  </React.StrictMode>
+)
